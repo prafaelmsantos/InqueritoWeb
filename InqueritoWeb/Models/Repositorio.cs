c9b95d24b6fc130c71df84f9495d4f6edcd0e443
Rafael Santos <prafaelmsantos@gmail.com>
2023-03-12 23:39:46 +0000

    12/03/2023

---
+++ InqueritoWeb/Models/Repositorio.cs
@@ -0,0 +1,22 @@
+﻿namespace InqueritoWeb.Models
+{
+    public class Repositorio
+    {
+        private static List<Resposta> respostas = new List<Resposta>();
+        public static IEnumerable<Resposta> Respostas { get { return respostas; } } // Nao permite remover ou editar
+        public static void AdicionarResposta(Resposta resposta)
+        {
+            respostas.Add(resposta);
+        }
+
+        static Repositorio()
+        {
+            respostas.Add(new Resposta()
+            { Nome = "Maria", Email = "maria@gmail.com", Sim = false });
+            respostas.Add(new Resposta()
+            { Nome = "José", Email = "jose@gmail.com", Sim = true });
+            respostas.Add(new Resposta()
+            { Nome = "João", Email = "joao@gmail.com", Sim = false });
+        }
+    }
+}
---
+++ InqueritoWeb/Models/Resposta.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InqueritoWeb.Models
+{
+    public class Resposta
+    {
+        [Required(ErrorMessage = "O campo Nome deve ser preenchido!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo E-mail deve ser preenchido!")]
+        [EmailAddress(ErrorMessage = "O campo Email não corresponde a um endereço válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Resposta deve ser preenchido!")]
+        public bool? Sim { get; set; }
+    }
+}
